03a91f91f946dee3d36e8c14f71fc136e4668fbc
442203479 <442203479@student.ksu.edu.sa>
2023-09-11 10:53:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/software3/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.software3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        TextView receiver_msg = (TextView) findViewById(R.id.received_value_id);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("message_key");
+        receiver_msg.setText("Hello "+str+"!");
+    }
+}
